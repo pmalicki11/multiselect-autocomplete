5f9c168058956940370dde3084d152aae46ebee5
pmalicki11 <pmalicki11@gmail.com>
2020-09-13 18:19:44 +0200

    Selecting and removing

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ https://pmalicki11.github.io/multiselect-autocomplete/
 Or here*
 https://jsfiddle.net/gh/get/library/pure/pmalicki11/multiselect-autocomplete/tree/master/jsFiddle
 
-*Preview does not work in Firefox and IE (those I tested)
+*Preview may not work in Firefox and IE (those I tested)
--- jsFiddle/demo.js
+++ jsFiddle/demo.js
@@ -1,73 +1,55 @@
-//const hardCodedItems = ["Mleko","Miód", "Orzechy arachidowe","Olej kokosowy","Orzechy laskowe","Benzoensan sodu"];
-
 document.addEventListener('DOMContentLoaded', function() {
-  const multiselectTextInput = document.querySelector('.multiselect-text-input');
-  multiselectTextInput.addEventListener('input', e => {
-    if(!e.target.value == "")
-      requestItems(e.target.value);
-      //generateMultiselectDropdownList(hardCodedItems);
-    else
-      clearMultiselectDropdownList();
-  });
-
-  
   let highlightedDropdownItem = null;
+  const multiselectinputWrapper = document.querySelector('.multiselect-input-wrapper');
+  const multiselectSelectedItems = document.querySelector('.multiselect-selected-list');
+  const multiselectTextInput = document.querySelector('.multiselect-text-input');
   const multiselectDropdownList = document.querySelector('.multiselect-dropdown-list');
+
+
+  multiselectinputWrapper.addEventListener('click', focusOnInput);
+
+
+  multiselectTextInput.addEventListener('input', (e) => {
+    if(!e.target.value == '') {
+      requestItems(e.target.value);
+    } else {
+      clearMultiselectDropdownList();
+    }
+  });
   
-
-  function generateMultiselectDropdownList(items) {
-    clearMultiselectDropdownList();
-    let dropdownElements = '';
-    items.forEach((item, i) => {
-      dropdownElements += `<li class="multiselect-dropdown-item" data-index="${i}">${item}</li>`;
-    });
-
-    multiselectDropdownList.innerHTML = dropdownElements;
-
-    Array.from(multiselectDropdownList.children).forEach(li => {
-      li.addEventListener('mouseover', e => {  
-        highlightDropdownItem(e.target);
-      });
-    });
-
-    highlightDropdownItem(multiselectDropdownList.children[0]);
-  }
-
-
   function requestItems(inputString) {
     const requestAddress = `https://pmalicki.com/alergens/products/ajax?namepart=${inputString}`;
     fetch(requestAddress)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       generateMultiselectDropdownList(data);
     })
-    .catch((e) => multiselectDropdownList.innerHTML = '<li>Nothing found</li>');
-    multiselectDropdownList.innerHTML = '<li>Searching...</li>';
+    .catch(showNothingFound);
+    showSearchingInfo();
   }
 
 
-  multiselectTextInput.addEventListener('keydown', e => {
-    if(e.key === 'Escape') {
-      clearMultiselectDropdownList();
-    } else if(e.key === 'ArrowUp') {
-      e.preventDefault();
-      highlightPreviousDropdownItem();
-    } else if(e.key === 'ArrowDown') {
-      e.preventDefault();
-      highlightNextDropdownItem();
-    }
-  }, false);
+  function generateMultiselectDropdownList(items) {
+    clearMultiselectDropdownList();
+    let dropdownElements = '';
+    items.forEach((item, i) => {
+      if(!isItemSelected(item)) {
+        dropdownElements += `<li class="multiselect-dropdown-item" data-index="${i}">${item}</li>`;
+      }
+    });
 
-
-  function showSearchingInfo() {
+    multiselectDropdownList.innerHTML = dropdownElements;
     
+    Array.from(multiselectDropdownList.children).forEach((li) => {
+      li.addEventListener('mouseover', (e) => {  
+        highlightDropdownItem(e.target);
+      });
+      li.addEventListener('click', (e) => { 
+        addToSelectedItems(e.target.innerHTML);
+      });
+    });
+    highlightDropdownItem(multiselectDropdownList.children[0]);
   }
-
-  function clearMultiselectDropdownList() {
-    multiselectDropdownList.innerHTML = "";
-  }
-
 
   function highlightDropdownItem(item) {
     if(highlightedDropdownItem != null)
@@ -101,4 +83,85 @@ document.addEventListener('DOMContentLoaded', function() {
       highlightedDropdownItem.classList.add('multiselect-dropdown-item-highlighted');
     }
   }
+
+
+  function clearMultiselectDropdownList() {
+    multiselectDropdownList.innerHTML = '';
+  }
+
+  
+  function showSearchingInfo() {
+    multiselectDropdownList.innerHTML = '<li>Searching...</li>';
+  }
+
+
+  function showNothingFound() {
+    multiselectDropdownList.innerHTML = '<li>Nothing found</li>'
+  }
+
+
+  multiselectTextInput.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape') {
+      clearMultiselectDropdownList();
+    } else if(e.key === 'ArrowUp') {
+      e.preventDefault();
+      highlightPreviousDropdownItem();
+    } else if(e.key === 'ArrowDown') {
+      e.preventDefault();
+      highlightNextDropdownItem();
+    } else if(e.key === 'Enter') {
+      e.preventDefault();
+      if(highlightedDropdownItem) {
+        addToSelectedItems(highlightedDropdownItem.innerHTML);
+      }
+    }
+  });
+
+
+  function addToSelectedItems(item) {
+    const li = document.createElement('li');
+    const i = document.createElement('i');
+    
+    li.classList.add('multiselect-selected-item');
+    li.innerHTML = `<span>${item}</span>`;
+    i.classList.add('material-icons');
+    i.innerHTML = 'clear';
+    i.addEventListener('click', removeSelectedItem);
+
+    li.appendChild(i);
+    multiselectSelectedItems.appendChild(li);
+    clearInput();
+    focusOnInput();
+  }
+
+
+  function clearInput() {
+    multiselectTextInput.value = '';
+    clearMultiselectDropdownList();
+  }
+
+
+  function focusOnInput() {
+    multiselectTextInput.focus();
+  }
+
+
+  function removeSelectedItem() {
+    multiselectSelectedItems.removeChild(this.parentNode);
+  }
+
+
+  function isItemSelected(item) {
+    let isSelected = false;
+    if(multiselectSelectedItems.children.length > 0) {
+      Array.from(multiselectSelectedItems.children).forEach((li) => {
+        if(li.firstChild.innerHTML == item) {
+          isSelected = true;
+          return;
+        }
+      });
+    }
+    return isSelected;
+  }
+
 });
